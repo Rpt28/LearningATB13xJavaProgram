da1670d4bbbda3bcb14e995ee9f6ebe9686dabe9
Rpt28 <rahultiwaricool28@gmail.com>
2025-07-03 14:31:48 +0530

    3 July 2025 - Polymorphism, Encapsulation

---
+++ src/ex_20_Polymorphism/Lab097_MethodOverriding.java
@@ -0,0 +1,36 @@
+package ex_20_Polymorphism;
+
+public class Lab097_MethodOverriding {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.sound();
+    }
+}
+
+class Animal {
+
+    void sound() {
+        System.out.println("Default Sound!");
+    }
+
+    void A() {
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Bark!!");
+    }
+
+    void methodOverLoad() {
+
+    }
+
+    void methodOverLoad(int a) {
+
+    }
+
+
+}
